267d2eac15b3f23c198641121d90355aa6034656
Mark Gildea <gildeamark1@gmail.com>
2016-12-15 21:40:35 -0600

    vars

---
+++ liri.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var spotify = require('spotify');
+
+var Twitter = require('twitter');
+var twitterKeys = require('./keys.js');
+var myKeys = twitterKeys.twitterKeys;
+var myTwitter = new Twitter({
+	consumer_key: myKeys.consumer_key,
+	consumer_secret: '',
+	access_token_key: '',
+	access_token_secret: '',
+});
+
+var fs = require('fs');
+
+var command = process.argv[2];
