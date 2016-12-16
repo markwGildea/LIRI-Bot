c156c7925714a0be75fb9cb3e3e1abb706f9ff7a
Mark Gildea <gildeamark1@gmail.com>
2016-12-16 12:18:27 -0600

    new Twitter

--- liri.js
+++ liri.js
@@ -6,9 +6,9 @@ var twitterKeys = require('./keys.js');
 var myKeys = twitterKeys.twitterKeys;
 var myTwitter = new Twitter({
 	consumer_key: myKeys.consumer_key,
-	consumer_secret: '',
-	access_token_key: '',
-	access_token_secret: '',
+	consumer_secret: myKeys.consumer_secret,
+	access_token_key: myKeys.access_token_key,
+	access_token_secret: myKeys.access_token_secret,
 });
 
 var fs = require('fs');
