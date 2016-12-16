d444b7e630062c733573ab12ee440c7bece75164
Mark Gildea <gildeamark1@gmail.com>
2016-12-15 18:24:34 -0600

    twitter

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'sWoND8ZJs9rY9U53f0A0UBhD0',
+  consumer_secret: 'CwTTjctvl7OY3YiZd4cn5odQQr9QrohcN48aGygLF71viGXljI',
+  access_token_key: '2244203202-Mk9WYnyJa0j9rGP4BTp258j75wXSTN9YDLr200d',
+  access_token_secret: 'RXcj1bmuocJebYYyqp0bKbtNjSaZhsbBIG34BRMeFquSi',
 };
